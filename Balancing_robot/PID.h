328b1b717e02b14641c2f321457a708f0355857c
TAndy1115 <andytangtsk@gmail.com>
2023-04-26 06:56:27 +0000

    This is it for today...

--- Balancing_robot/PID.h
+++ Balancing_robot/PID.h
@@ -4,11 +4,12 @@ class PID {
     public: 
         float Kp, Ki, Kd;
         void initPID(float p, float i, float d, float period);
+        float updatePID(float process_variable, float set_point);
     private:
         uint16_t max, error_division_factor;
-        double set_point, dead_zone, error, p_error, error_d, error_sum, out;
+        double dead_zone, error, p_error, error_d, error_sum, out;
         volatile bool PID_flag;
         uint32_t PID_elapsed_time;
         void update();
-        float updatePID();
+        Ticker loop;
 };
